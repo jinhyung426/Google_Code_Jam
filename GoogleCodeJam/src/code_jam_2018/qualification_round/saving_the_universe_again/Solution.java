1a893631aaa7ec360407b75089b8b68c231b2b7d
Jason Park <jh960426@naver.com>
2020-03-06 23:27:54 -0500

    Added Hierarchial Folders to code_jam_2018

---
+++ GoogleCodeJam/src/code_jam_2018/qualification_round/saving_the_universe_again/Solution.java
@@ -0,0 +1,10 @@
+package code_jam_2018.qualification_round.saving_the_universe_again;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GoogleCodeJam/src/code_jam_2018/qualification_round/trouble_sort/Solution.java
@@ -0,0 +1,10 @@
+package code_jam_2018.qualification_round.trouble_sort;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
